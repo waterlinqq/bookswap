722bcfa1f7b21fd6382c7623ac62acb5b0114872
waterlinqq <a10311036@gmail.com>
2020-04-13 16:18:31 +0800

    feat(app): pug template

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
+const shopRouter = require("./routes/shop");
+
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use("/shop", shopRouter);
 app.use((req, res, next) => {
   res.render("index");
 });
