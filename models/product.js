0fbd2ef07b2e057349a9c95bfe2406a6beb5762d
waterlinqq <a10311036@gmail.com>
2020-04-13 18:07:11 +0800

    feat(product): class product getall data

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const rootDir = require("../utils/path");
+const filePath = path.join(rootDir, "data", "products.json");
 
 class Product {
   constructor(title, url, price, description) {
@@ -12,7 +13,6 @@ class Product {
     this.description = description;
   }
   save() {
-    const filePath = path.join(rootDir, "data", "products.json");
     const readCb = (err, data) => {
       if (err) data = "[]";
       const content = JSON.parse(data.toString());
@@ -20,6 +20,14 @@ class Product {
       fs.writeFile(filePath, JSON.stringify(content), console.log);
     };
     fs.readFile(filePath, readCb);
+  }
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) reject([]);
+        else resolve(JSON.parse(data.toString()));
+      });
+    });
   }
 }
 
