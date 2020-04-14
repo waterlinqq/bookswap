353bac63651e11b5ac71e0913e206c5d37680b07
waterlinqq <a10311036@gmail.com>
2020-04-14 20:05:44 +0800

    feat(product): model add modById method

--- models/product.js
+++ models/product.js
@@ -41,6 +41,20 @@ class Product {
       });
     });
   }
+  static modById(id, props) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) reject(null);
+        else {
+          const products = JSON.parse(data.toString());
+          const product = products.find((p) => p.id === id);
+          Object.assign(product, props);
+          fs.writeFile(filePath, JSON.stringify(products), console.log);
+          resolve(product);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Product;
