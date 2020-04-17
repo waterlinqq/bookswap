a622eb49155c5d67f5403625f1d0f6e3a0c75389
waterlinqq <a10311036@gmail.com>
2020-04-17 15:06:52 +0800

    feat(product): define sequlize model

--- models/product.js
+++ models/product.js
@@ -1,74 +1,27 @@
-const fs = require("fs");
-const path = require("path");
+const Sequelize = require("sequelize");
 
-const rootDir = require("../utils/path");
-const filePath = path.join(rootDir, "data", "products.json");
+const sequelize = require("../utils/database");
 
-class Product {
-  constructor(title, url, price, description) {
-    this.id = Math.random().toString();
-    this.title = title;
-    this.url = url;
-    this.price = price;
-    this.description = description;
-  }
-  save() {
-    const readCb = (err, data) => {
-      if (err) data = "[]";
-      const content = JSON.parse(data.toString() || "[]");
-      content.push(this);
-      fs.writeFile(filePath, JSON.stringify(content), console.log);
-    };
-    fs.readFile(filePath, readCb);
-  }
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) reject([]);
-        else resolve(JSON.parse(data.toString()));
-      });
-    });
-  }
-  static getById(id) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) reject(null);
-        else {
-          const products = JSON.parse(data.toString());
-          const product = products.find((p) => p.id === id);
-          resolve(product);
-        }
-      });
-    });
-  }
-  static modById(id, props) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) reject(null);
-        else {
-          const products = JSON.parse(data.toString());
-          const product = products.find((p) => p.id === id);
-          Object.assign(product, props);
-          fs.writeFile(filePath, JSON.stringify(products), console.log);
-          resolve(product);
-        }
-      });
-    });
-  }
-  static delById(id) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) reject(err);
-        else {
-          const products = JSON.parse(data.toString());
-          const index = products.findIndex((p) => p.id === id);
-          products.splice(index, 1);
-          fs.writeFile(filePath, JSON.stringify(products), console.log);
-          resolve(true);
-        }
-      });
-    });
-  }
-}
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  url: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
 module.exports = Product;
