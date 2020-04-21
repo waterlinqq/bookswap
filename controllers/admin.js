cdf9552688e3f867f5542dc710007ecf0928334c
waterlinqq <a10311036@gmail.com>
2020-04-21 13:24:23 +0800

    feat(admin): validate admin operation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator/check");
+
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
@@ -6,6 +8,13 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = async (req, res, next) => {
   const { title, price, description, url } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(422).render("admin/add-product", {
+      value: { title, price, description, url },
+      errorMessage: errors.array()[0].msg,
+      errorParam: errors.array()[0].param,
+    });
   await req.user.createProduct({ title, price, description, url });
   res.redirect("/admin/products");
 };
@@ -17,14 +26,20 @@ exports.getProducts = async (req, res, next) => {
 
 exports.getEditProduct = async (req, res, next) => {
   const id = req.params.productId;
-  const prods = await req.user.getProducts({ where: { id } });
-  const prod = prods[0];
+  const [prod] = await req.user.getProducts({ where: { id } });
   if (prod == null) res.redirect("/");
   res.render("admin/edit-product", { prod });
 };
 
 exports.postEditProduct = async (req, res, next) => {
   const { id, title, url, price, description } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(422).render("admin/add-product", {
+      value: { title, url, price, description },
+      errorMessage: errors.array()[0].msg,
+      errorParam: errors.array()[0].param,
+    });
   const prod = await Product.findByPk(id);
   if (prod.userId.toString() !== req.user.id.toString())
     return res.redirect("/");
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const { body } = require("express-validator/check");
 
 const router = express.Router();
 
 const adminController = require("../controllers/admin");
 const isAuth = require("../middleware/is-auth");
 
+const checkProduct = [
+  body("title").isString().isLength({ min: 3 }).trim(),
+  body("url").isURL(),
+  body("price").isNumeric(),
+  body("description").isLength({ max: 400 }).trim(),
+];
+
 router.get("/add-product", isAuth, adminController.getAddProduct);
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  checkProduct,
+  isAuth,
+  adminController.postAddProduct
+);
 router.get("/products", isAuth, adminController.getProducts);
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  checkProduct,
+  isAuth,
+  adminController.postEditProduct
+);
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
