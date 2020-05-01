08a332f2f1c24f4ef17ac643b442d2729c589535
waterlinqq <a10311036@gmail.com>
2020-05-01 17:50:31 +0800

    fix(mysql): set charset utf8_general_cli

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("bookswap", "root", "", {
   dialect: "mysql",
   host: "localhost",
+  charset: "utf8",
+  collate: "utf8_general_ci",
 });
 
 module.exports = sequelize;
