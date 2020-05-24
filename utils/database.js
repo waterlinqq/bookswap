a3b55ce4d7323d531064ad94aa310862c02a3c98
waterlinqq <a10311036@gmail.com>
2020-05-24 22:15:50 +0800

    feat(app): prepare to publish

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bookswap", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-  charset: "utf8",
-  collate: "utf8_general_ci",
-});
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USERNAME,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.MYSQL_HOST,
+    charset: "utf8",
+    collate: "utf8_general_ci",
+  }
+);
 
 module.exports = sequelize;
